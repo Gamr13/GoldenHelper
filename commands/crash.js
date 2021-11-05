cf46de9288c365b00443d908b6221600ff25ee9b
Gamr13 <48205338+Gamr13@users.noreply.github.com>
2021-11-05 11:10:26 +0000

    Add files via upload

---
+++ commands/crash.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('crash')
+        .setDescription('Find out a list of known RetroArch crashes and fixes!'),
+    async execute(interaction) {
+        interaction.reply({ embeds: [exampleEmbed] })
+    }
+}
+	
+const exampleEmbed = new MessageEmbed()
+	.setColor('#FFD700')
+	.setTitle('RetroArch Crashes.')
+	.setAuthor('GoldenHelper', 'https://i.imgur.com/o7MkhhK.png')
+	.setDescription("")
+	.setTimestamp()
+	.setFooter('© GoldenSky#6969 | 2021', 'https://i.imgur.com/I2LsE5o.png');
+

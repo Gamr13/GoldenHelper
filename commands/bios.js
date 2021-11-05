498a3940de0d9093351e15ec89e73cbc611f7dab
Gamr13 <48205338+Gamr13@users.noreply.github.com>
2021-11-05 22:11:25 +0000

    Changed the RichEmbed title

--- commands/bios.js
+++ commands/bios.js
@@ -10,12 +10,11 @@ module.exports = {
         interaction.reply({ embeds: [exampleEmbed] })
     }
 }
-	
+
 const exampleEmbed = new MessageEmbed()
 	.setColor('#FFD700')
-	.setTitle('Whitelist Application.')
+	.setTitle('BIOS file locations')
 	.setAuthor('GoldenHelper', 'https://i.imgur.com/o7MkhhK.png')
 	.setDescription("Before reading this, make sure you know how to connect to your Xbox by following /ftp or /mfe first!\n\n1) Connect to your Xbox and open LOCALFOLDER > The RetroArch folder\n2) Open the System folder\n3) Place all of your BIOS files **EXCEPT** your PS2 and Wii/GC BIOS files in here.\n4) Create a pcsx2 folder and a bios folder inside that, now place your PS2 BIOS in here.\n5) Go back to system and open / create the Dolphin-emu folder, place the Sys folder in here.\n\n And that's it, you're done!")
 	.setTimestamp()
 	.setFooter('© GoldenSky#6969 | 2021', 'https://i.imgur.com/I2LsE5o.png');
-

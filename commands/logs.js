e6632cc4c667068177fad40526eef09848841395
Gamr13 <48205338+Gamr13@users.noreply.github.com>
2021-11-11 10:52:25 +0000

    Add files via upload

--- commands/logs.js
+++ commands/logs.js
@@ -18,8 +18,8 @@ const exampleEmbed = new MessageEmbed()
 	.setDescription('Here is how to enable logs and send it to us:')
 	.addFields(
 		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Step 1', value: 'Inside RetroArch, go to Settings > Logging and [enable the following settings](https://i.imgur.com/T4YdaBI.png)', inline: true },
-		{ name: 'Step 2', value: 'Go back to the Main Menu in RetroArch (Farthest left in XMB) and go into Configuration File, save the current config and quit RetroArch (from inside RetroArch).', inline: true },
+		{ name: 'Step 1', value: 'Inside RetroArch, go to Settings > Logging and [enable the following settings](https://i.imgur.com/T4YdaBI.png)', inline: false },
+		{ name: 'Step 2', value: 'Go back to the Main Menu in RetroArch (Farthest left in XMB) and go into Configuration File, save the current config and quit RetroArch (from inside RetroArch).', inline: false },
 		{ name: 'Step 3', value: 'Go back into RetroArch and re-create the steps you took to create the error. Once you have done that, Quit RetroArch again, from inside RetroArch.', inline: false },
 		{ name: 'Step 3', value: 'Follow /ftp or the MC Addons Manager guide in /videos to get to the RetroArch LocalState folder. From here, go to the Logs folder and send us the log inside.', inline: false },
 	)

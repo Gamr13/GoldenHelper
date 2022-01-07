c4da7a24909a1e0d153e40bb477c508c8744c8b5
Gamr13 <48205338+Gamr13@users.noreply.github.com>
2022-01-07 22:52:49 +0000

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -21,7 +21,7 @@ const exampleEmbed = new MessageEmbed()
 		{ name: 'Remove another device error?', value: '[Click here](https://account.microsoft.com/devices/content) and remove devices from the Microsoft Store', inline: false },
 		{ name: 'What do I do when RetroArch is removed from the store?', value: 'Wait for my re-uploaded announcement.', inline: false },
 		{ name: 'How do I backup my saves and data?', value: 'Do /backup', inline: false },
-		{ name: 'Will I need to apply to the whitelist after a takedown?', value: 'No.', inline: false },
+		{ name: 'Can I continue to use the currently installed apps without reinstalling after a takedown?', value: 'Yes.', inline: false },
 		{ name: 'How do I fix black screen on Mupen64plus?', value: 'Quick Menu > Options > RDP Plugin > Angrylion', inline: false },
 		{ name: 'How do I fix no input in Wii games?', value: 'Quick Menu > Controls > Disable the 5th port.', inline: false },
 	)

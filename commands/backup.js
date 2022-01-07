2db8c62d7881b23eaea8c28562bfc19a2aecb2a1
Gamr13 <48205338+Gamr13@users.noreply.github.com>
2022-01-07 22:55:27 +0000

    Update backup.js

--- commands/backup.js
+++ commands/backup.js
@@ -18,7 +18,7 @@ const exampleEmbed = new MessageEmbed()
 	.setDescription('Welcome to the Backup section. Follow /ftp to find out how to connect to your Xbox in retail or dev mode.')
 	.addFields(
 		{ name: 'Backing up RetroArch:', value: '- config\n- playlists\n- saves\n- states\n- system\n- content_history.lpl\n It is recommended to not backup retroarch.cfg', inline: true },
-		{ name: 'Backing up PPSSPP:', value: '- PSP\n- Test\n- Test', inline: true },
+		//{ name: 'Backing up PPSSPP:', value: '- PSP\n- Test\n- Test', inline: true },
 	)
 	.setTimestamp()
 	.setFooter('© GoldenSky#6969 | 2021', 'https://i.imgur.com/I2LsE5o.png');

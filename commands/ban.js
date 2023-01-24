f9d555a177c3e0eb1a36f6cec0bbb170f88de470
Gamr13 <48205338+Gamr13@users.noreply.github.com>
2023-01-24 11:01:20 +0000

    Add files via upload

--- commands/ban.js
+++ commands/ban.js
@@ -1,19 +1,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { EmbedBuilder, PermissionsBitField } = require('discord.js');
+const { EmbedBuilder, PermissionsBitField, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('ban')
     .setDescription('Slam the ban hammer on a member.')
     .addUserOption(option => option.setName('target').setDescription('Who do I ban?').setRequired(true))
-    .addStringOption(option => option.setName('reason').setDescription('Why are they being banned?')),
+    .addStringOption(option => option.setName('reason').setDescription('Why are they being banned?'))
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers),
+
     async execute (interaction, client) {
 
         const banUser = interaction.options.getUser('target');
         const banMember = await interaction.guild.members.fetch(banUser.id);
         const channel = interaction.channel;
 
-        if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) return await interaction.reply({ content: "You must have the ban Members permission to use this command", ephemeral: true });
+        if (!interaction.member.permissions.has(PermissionsBitField.Flags.BanMembers)) return await interaction.reply({ content: "Error: You don't have permission to use this command.", ephemeral: true });
         if (!banMember) return await interaction.reply({ content: 'Error: User is not in the server.', ephemeral: true});
         if (!banMember.kickable) return await interaction.reply({ content: "Error: This person is above you in roles.", ephemeral: true});
 
@@ -22,7 +24,6 @@ module.exports = {
 
         const dmEmbed = new EmbedBuilder()
         .setColor("DarkOrange")
-        //.setDescription(`:white_check_mark:  You've been permanently banned from: **${interaction.guild.name}**`)
         .addFields(
             { name: ':white_check_mark:', value: `Your ass got perma-banned from **${interaction.guild.name}**`, inline: false },
             { name: 'Reason:', value: `${reason}`, inline: false },
@@ -40,7 +41,7 @@ module.exports = {
         });
 
         await banMember.ban({ reason: reason }).catch(err => {
-            interaction.reply({ content: `There was an error: $err`, ephemeral: true});
+            interaction.reply({ content: `There was an error.`, ephemeral: true});
         });
 
         await interaction.reply({ embeds: [embed] });

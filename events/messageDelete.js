1e007f244ec95f35ae20b4739e76d5affacb57c5
Gamr13 <48205338+Gamr13@users.noreply.github.com>
2023-03-10 21:58:57 +0000

    Add files via upload
    Add return for un-cached messages

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -4,8 +4,10 @@ const { client } = require("../index.js");
 module.exports = {
     name: "messageDelete",
     once: false,
+	
 
     execute (message) {
+		if (!message.member) return;
         let embed = new EmbedBuilder()
         .setColor('#920dff')
         .setTitle(`Message Deleted`)

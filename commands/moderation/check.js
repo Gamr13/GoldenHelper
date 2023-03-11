70d2fe2f9d782581bcb5e19a4a012c3ce17167fb
Gamr13 <48205338+Gamr13@users.noreply.github.com>
2023-03-11 22:16:04 +0000

    Update check.js

--- commands/moderation/check.js
+++ commands/moderation/check.js
@@ -38,7 +38,6 @@ module.exports = {
         .setAuthor({name: 'GoldenHelper', iconURL:"https://i.imgur.com/o7MkhhK.png"})
         .setDescription(`:white_check_mark: Information for: ${user.user.tag} (ID: ${user.id} )`)
         .setThumbnail(`${user.displayAvatarURL()}`)
-        .addField("Bot:", `${user.bot}`, true)
         .addField(":triangular_flag_on_post: Strikes: ", `${content}`, true )
         //.addFields(
           //  { name: ':triangular_flag_on_post: Strikes: ', value: `${content}`, inline: true }

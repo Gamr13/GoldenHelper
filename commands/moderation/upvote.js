c9ad411a70b64c87e53d94016f5caca033cedaa9
Adrian Kuśmierek <51207265+AdrianKusmierek@users.noreply.github.com>
2023-03-11 20:56:22 +0100

    Update upvote.js

--- commands/moderation/upvote.js
+++ commands/moderation/upvote.js
@@ -58,7 +58,7 @@ module.exports = {
                 .setAuthor({name: 'GoldenHelper', iconURL:"https://i.imgur.com/o7MkhhK.png"})
                 .setDescription(`<@${user.user.id}> has ranked up to <@&${rep[0][k]}>`);
                 console.log(1);
-                let role = client.guilds.roles.fetch(rep[0][k])
+                let role = interaction.guild.roles.fetch(rep[0][k])
                 .then(role => {
                     console.log(2);
                     console.log(role)

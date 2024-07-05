a45119023c7d63b46b9099dcde94b852141a31d9
GoldenSky <48205338+Gamr13@users.noreply.github.com>
2024-07-05 07:14:26 +0100

    Update customization.js
    Remove duplicate text

--- commands/client-customs/customization.js
+++ commands/client-customs/customization.js
@@ -17,7 +17,6 @@ const exampleEmbed = new EmbedBuilder()
 	.setDescription('Welcome to the RetroArch Customization Guide.')
 		.addFields(
 			{ name: 'Customizing RetroArch', value: 'After changing customization settings in RetroArch, make sure you go to Main Menu > Configuration File > Save Current Configuration and **restart RetroArch** for your changes to take effect.\n\n**Menu Driver:**\n- To change the Menu Driver, go to Settings > Drivers > Menu and select an option. \n\n- Changing the colour scheme of RetroArch can be done in Settings > User Interface > Appearance > Menu Color Theme', inline: false },
-			{ name: 'Menu Driver:', value: '- To change the Menu Driver, go to Settings > Drivers > Menu and select an option.', inline: false },
 			{ name: 'Changing Colour Scheme:', value: '- Changing the colour scheme of RetroArch can be done in Settings > User Interface > Appearance > Menu Color Theme', inline: false },
 		)
 	.setTimestamp()
